0d6b89b8b61d568b159412ccdc6315a666d4ad5f
ahyush <ahyush@noreply.codeberg.org>
2024-05-30 00:12:18 -0500

    #5 Added function name in call expressions census. (WIP)

--- clang/tools/clang-tools-extra/cast-chk/utils.h
+++ clang/tools/clang-tools-extra/cast-chk/utils.h
@@ -18,6 +18,9 @@
 #include "clang/AST/Expr.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/Stmt.h"
+#include "clang/AST/ParentMapContext.h"
+
+#include "clang/Basic/IdentifierTable.h"
 
 #include <iostream>
 #include <fstream>
@@ -36,7 +39,7 @@ using namespace clang::ento;
 
 // Stringer to get source statement from Stmt.
 std::string toString(ASTContext *context, DeclStmt const *decl);
-std::string toString(ASTContext *context, CallExpr const *call);
+std::string toString(ASTContext *context, CallExpr const *call, unsigned parmPos);
 std::string toString(ASTContext *context, Stmt const *stmt);
 // Get type name of Expr.
 std::string typeof(ASTContext *context, QualType qtype);
@@ -46,6 +49,8 @@ std::string typeof(ASTContext *context, Expr const *expr);
 std::string typeof(ASTContext *context, clang::Type const *type);
 // Get type class of Expr (pointer, array)
 std::string getTypeCategoryName(ASTContext *context, Expr const *expr);
+
+std::string getContainerFunction(ASTContext *context, clang::Stmt const *stmt);
 
 // Stringer to get source statement from Stmt.
 std::string toString(ASTContext *context, Stmt const *stmt) {
@@ -76,13 +81,44 @@ std::string toString(ASTContext *context, DeclStmt const *decl) {
     return "(Could not find name!)";
 }
 
-std::string toString(ASTContext *context, CallExpr const *call) {
+std::string toString(ASTContext *context, CallExpr const *call, unsigned parmPos) {
     assert(context);
     assert(call);
     auto const * fn = call->getDirectCallee();
     if(!fn)
         return "(Could not find function name!)";
-    return fn->getNameAsString();
+
+    std::stringstream ss;
+    ss << fn->getNameAsString() << "{$" << parmPos << ": ";
+
+    /*
+    auto const * fnDecl = call->getDirectCallee();
+    if(!fnDecl) {
+        ss << "(Cannot getDirectCallee())";
+        return ss.str();
+    }
+    assert(fnDecl);
+    */
+    auto const * parm = fn->getParamDecl(parmPos);
+    if(!parm) {
+        ss << "(Cannot getParamDecl())";
+        return ss.str();
+    }
+    assert(parm);
+    // Get parm type
+    auto const parmType = parm->getOriginalType(); 
+    ss << typeof(context, parmType) << "} ";
+
+    // Get parm id
+    auto const * parmId = parm->getIdentifier();
+    if(!parmId) {
+        ss << "(Cannot get parameter ID)";
+        return ss.str();
+    }
+    assert(parmId);
+    ss << parmId->getName().str();
+
+    return ss.str();
 }
 
 // Get type name of Expr.
@@ -124,5 +160,40 @@ std::string getTypeCategoryName(ASTContext *context, Expr const *expr) {
 }
 
 // Get containing function for declaration
+std::string getContainerFunction(ASTContext *context, clang::Stmt const *stmt) {
+    assert(context);
+    assert(stmt);
+
+    auto parents = context->getParents(*stmt);
+    if (parents.size() == 0) {
+        return "(0 Parents found)";
+    }
+
+    /*
+    for(auto const& parent: parents) {
+        auto const * fn = parent.get<clang::FunctionDecl>();
+        if(fn && isa<clang::FunctionDecl>(fn)) {
+            return fn->getNameAsString();
+        }
+    */
+    while(parents[0].get<clang::FunctionDecl>() == nullptr) {
+        auto const * decl = parents[0].get<clang::Decl>();
+        auto const * stmt = parents[0].get<clang::Stmt>();
+        if(!decl && stmt) {
+            parents = context->getParents(*stmt);
+        }
+        else if(!stmt && decl) {
+            parents = context->getParents(*decl);
+        }
+        else
+            return "(Can't loop forever, gfy)";
+    }
+    auto const *fn = parents[0].get<clang::FunctionDecl>();
+    if(!fn) {
+        return "(Could not find container function)";
+    }
+    
+    return fn->getNameAsString();
+}
 
 // Get containing translation unit for declaration
