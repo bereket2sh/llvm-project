e546bcde8f57e66c27f59c9060e6de3c07bbb7de
ahyush <ahyush@noreply.codeberg.org>
2024-11-27 10:39:41 -0600

    Updated hof test. (WIP) #14

--- clang/tools/clang-tools-extra/cast-chk/test/fp.c
+++ clang/tools/clang-tools-extra/cast-chk/test/fp.c
@@ -1,11 +1,17 @@
 void f(void *f_pv) {
+    char *xp = (char*) f_pv;
 }
 
 void g(void *g_pv) {
+    int *yp = (int*) g_pv;
 }
 
 void hof(void *x, void (*p)(void*)) {
     p(x);
+}
+
+void h(void *f_pv) {
+    int *zp = (int*) f_pv;
 }
 
 int main() {
@@ -13,11 +19,15 @@ int main() {
     int *pi = &i;
 
     void (*fp)(void*) = f;
-    fp(pi);
+    //fp(pi);
+
+    f(pi);
 
     hof(pi, fp);
+    //fp = h;
+    //hof(pi, fp);
 
-    //void (*fg)(void*) = g;
+    void (*fg)(void*) = g;
     hof(pi, g);
 
     return 0;
